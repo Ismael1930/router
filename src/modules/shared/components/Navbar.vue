d587053bbfb3eac148fa39f219f3b85d0ce2404e
apolanco <apolanco@cdevteam.com>
2023-12-07 15:05:05 -0400

    primer commit

---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <RouterLink to="/">Pokemon List</RouterLink>
+        <RouterLink to="/id">Pokemon por id</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+div {
+    padding: 0 30px;
+}
+
+div a {
+    font-weight: bold;
+    color: #2c3e50;
+    margin:  0 10px;
+}
+
+div .router-link-exact-active {
+    color: #42b983;
+}
+</style>
